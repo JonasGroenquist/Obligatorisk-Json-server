fe2f38340bf2e6bf653413c0bb050bb4f1dd335d
Jonas Grønquist <jog002@edu.zealand.dk>
2024-09-30 10:18:59 +0200

    Add project files.

---
+++ Obligatorisk Json server/Input.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonServerOpg
+{
+    public class Input
+    {
+        public string Method { get; set; }
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public Input()
+        {
+        }
+
+        public Input(string method, int number1, int number2)
+        {
+            Method = method;
+            Num1 = number1;
+            Num2 = number2;
+
+        }
+    }
+}
---
+++ Obligatorisk Json server/Program.cs
@@ -0,0 +1,85 @@
+﻿using JsonServerOpg;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+Console.WriteLine("TCP server");
+
+TcpListener listener = new TcpListener(IPAddress.Any, 7);
+
+listener.Start();
+
+while (true)
+{
+
+
+    TcpClient socket = listener.AcceptTcpClient();
+    IPEndPoint clientEndPoint = socket.Client.RemoteEndPoint as IPEndPoint;
+    Console.WriteLine("Client connected: " + clientEndPoint.Address);
+
+
+    Task.Run(() => HandleClient(socket));
+
+
+}
+
+void HandleClient(TcpClient socket)
+{
+
+    NetworkStream ns = socket.GetStream();
+    StreamReader reader = new StreamReader(ns);
+    StreamWriter writer = new StreamWriter(ns);
+
+    while (socket.Connected)
+    {
+        Random random = new Random();
+        string? msg = reader.ReadLine();
+        Input? jsonMsg = null;
+        Console.WriteLine("test: " + msg);
+
+        if (msg.ToLower() == "stop")
+        {
+            writer.WriteLine("Server has been stopped");
+        }
+        else
+        {
+            try
+            {
+                jsonMsg = JsonSerializer.Deserialize<Input>(msg);
+            }
+            catch (Exception ex)
+            {
+                writer.WriteLine("Invalid input");
+            }
+
+            if (jsonMsg != null)
+            {
+                if (jsonMsg.Method.StartsWith("add"))
+                {
+                    int sum = jsonMsg.Num1 + jsonMsg.Num2;
+                    string resultMessage = "Result: " + sum;
+                    Console.WriteLine(resultMessage);
+                    writer.WriteLine(resultMessage);
+                    writer.Flush();
+
+                }
+                if (jsonMsg.Method.StartsWith("subtract"))
+                {
+                    int sum = jsonMsg.Num1 - jsonMsg.Num2;
+                    string resultMessage = "Result: " + sum;
+                    Console.WriteLine(resultMessage);
+                    writer.WriteLine(resultMessage);
+                    writer.Flush();
+                }
+            }
+            if (jsonMsg.Method.StartsWith("random"))
+            {
+                int randomNum = random.Next(jsonMsg.Num1, jsonMsg.Num2);
+                string number = "" + randomNum;
+                Console.WriteLine(number);
+                writer.WriteLine(number);
+                writer.Flush();
+            }
+        }
+    }
+}
